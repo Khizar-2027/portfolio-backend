395c25ca64459d37de0564aeab8aead7cacf216c
khizar <khizarkarge43@gamil.com>
2026-06-15 19:15:13 +0530

    Try Gmail SSL

--- portfolio_backend/deployment_settings.py
+++ portfolio_backend/deployment_settings.py
@@ -45,7 +45,9 @@ CORS_ALLOWED_ORIGINS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
+
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_RECEIVER = os.environ.get('EMAIL_RECEIVER', '')
